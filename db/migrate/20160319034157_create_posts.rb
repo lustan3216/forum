fd788cb00a0daad927f5acc6d361bee181d1b491
lululala <et3216@gmail.com>
2016-03-19 12:10:25 +0800

    posts categories comments migrate

---
+++ db/migrate/20160319034157_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.text :description
+      t.boolean :is_public
+      t.integer :post_id
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+    add_index :posts , :post_id
+  end
+end
